b3a38e03cab67bb3ed0d0690e6d600e4e48d515b
dipengithub <thapasansar044@gmail.com>
2021-04-25 18:31:41 +0545

    first commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,12 +2,22 @@ package main
 //go run .
 //go mod init github.com/dip/djhcudn
 import (
+	"github.com/dipengithub/golangweb/pkg/config"
 	handler "github.com/dipengithub/golangweb/pkg/handlers"
+	"github.com/dipengithub/golangweb/pkg/render"
+	"log"
 	"net/http"
 
 )
 
 func main(){
+	var app config.AppConfig
+	tc,err:=render.CreateTemplateCache()
+	if err!=nil{
+		log.Fatal("cannot create template cache")
+	}
+	app.TemplateCache=tc
+	render.NewTemplates(&app)
 	http.HandleFunc("/", handler.Home)
 	http.HandleFunc("/about", handler.About)
 	_=http.ListenAndServe(":8080",nil)
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -3,6 +3,7 @@ package render
 import (
 	"bytes"
 	"fmt"
+	"github.com/dipengithub/golangweb/pkg/config"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -12,19 +13,22 @@ import (
 var functions = template.FuncMap{
 	
 }
+var app *config.AppConfig
+
+func NewTemplates(a *config.AppConfig)  {
+	app=a
+
+}
 
 func RenderTemplate(w http.ResponseWriter, tmpl string){
-	tc,err:=CreateTemplateCache()
-	if err!=nil{
-		log.Fatal(err)
-	}
+	tc:=app.TemplateCache
 	t,ok:=tc[tmpl]
 	if !ok{
-		log.Fatal(err)
+		log.Fatal("could not get template cache")
 	}
 	buf:=new(bytes.Buffer)
 	_=t.Execute(buf,nil)
-	_,err=buf.WriteTo(w)
+	_,err:=buf.WriteTo(w)
 	if err!=nil{
 		fmt.Println("error",err)
 	}
